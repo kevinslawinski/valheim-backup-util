4b8fc4c9e84b3d5734982559bc658a80b4b836d8
Kevin Slawinski <kevin.slawinski@gmail.com>
2024-06-15 21:53:49 -0400

    add main menu with functional options

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 import json
 import os
 import shutil
+import time
 
-
+# file name of user configuration
 USER_CONFIG = 'config.json'
-
-def load_config(USER_CONFIG):
-  if not os.path.exists(USER_CONFIG):
-    print('Config file not found')
-    return None
-  with open(USER_CONFIG, 'r') as config:
-    return json.load(config)
-  
+ 
 def save_config(config):
   with open(USER_CONFIG, 'w') as config_file:
     json.dump(config, config_file, indent=2)
     
-def prompt_for_config():
+def generate_config():
   print('Generating new config...\n')
   world_file_name = input('Name of the Valheim world: ')
   local_path = os.path.join(os.getenv('USERPROFILE'), 'AppData', 'LocalLow', 'IronGate', 'Valheim', 'worlds_local')
@@ -29,7 +23,16 @@ def prompt_for_config():
   save_config(config)
   return config
 
-def copy_files():
+def load_config(USER_CONFIG):
+  print('Loading config file...')
+  if not os.path.exists(USER_CONFIG):
+    print('Config file not found')
+    return generate_config()
+  with open(USER_CONFIG, 'r') as config:
+    print('Config file loaded successfully. Navigating to main menu...')
+    return json.load(config)
+
+def upload_files():
   with open(USER_CONFIG, 'r') as c:
     config = json.load(c)
     world_file = config.get('world_file_name')
@@ -45,17 +48,34 @@ def copy_files():
       else:
         print(f'Could not find .db or .fwl file matching specified world name "{world_file}".')
     
+def download_files():
+  print('Not yet implemented')
 
 def main():
-  config = load_config(USER_CONFIG)
-  if not config:
-    config = prompt_for_config()
-  result = copy_files()
-  if not result:
-    print('Something went wrong')
-  else:
-    print('Files copied!')
-  
+  while True:
+    print('Valheim Backup Utility')
+    print('----------------------')
+    load_config(USER_CONFIG)
+    time.sleep(2)
+    print('\n----------------------')
+    print('(1) Upload world files to git repository')
+    print('(2) Download world files from git repository')
+    print('(3) Regenerate config')
+    
+    choice = input('Choose an option (1-3, 0 to exit): ')
+    
+    if choice == '1':
+      upload_files()
+    elif choice == '2':
+      download_files()
+    elif choice == '3':
+      generate_config()
+    elif choice == '0':
+      print('Goodbye!')
+      break
+    else:
+      print('Not an option. Try again.')
+
 main()
     
 # os.system('git pull')
