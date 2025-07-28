c6cd4ec4130640175c5f3211ee62baf22ceab25e
Kevin Slawinski <kevin.slawinski@gmail.com>
2025-07-28 00:42:36 -0400

    more logging and error handling

--- src/services/file_service.py
+++ src/services/file_service.py
@@ -14,20 +14,23 @@ class FileService:
                 logging.info(f'Copied {file} from {src} to {dst}')
             except FileNotFoundError as e:
                 logging.warning(f'File not found: {src_file}. Error: {e}')
-                raise
             except Exception as e:
-                logging.warning(f'Error copying file {file} from {src} to {dst}: {e}')
-                raise
+                logging.error(f'Error copying file {file} from {src} to {dst}: {e}')
     
     @staticmethod
     def sync_files(action):
-        config = ConfigService().get()
-        world_file = config.get('world_file_name')
-        local_path = config.get('local_path')
-        repo_path = config.get('repo_path')
-        files = [f'{world_file}.db', f'{world_file}.fwl']
-        
-        if action == 'upload':
-            FileService._copy_files(local_path, repo_path, files)
-        elif action == 'download':
-            FileService._copy_files(repo_path, local_path, files)
+        try:
+            config = ConfigService().get()
+            world_file = config.get('world_file_name')
+            local_path = config.get('local_path')
+            repo_path = config.get('repo_path')
+            files = [f'{world_file}.db', f'{world_file}.fwl']
+
+            if action == 'upload':
+                FileService._copy_files(local_path, repo_path, files)
+            elif action == 'download':
+                FileService._copy_files(repo_path, local_path, files)
+            else:
+                logging.warning(f'Unknown action: {action}')
+        except Exception:
+            logging.error("An error occurred while syncing files.")
