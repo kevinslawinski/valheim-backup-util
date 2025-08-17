9f9aa7ad981fcd7b3618e24072c6ba52a246af61
Kevin Slawinski <kevin.slawinski@gmail.com>
2025-08-17 16:11:25 -0400

    some SOLID stuff

--- src/ui/gui.py
+++ src/ui/gui.py
@@ -3,53 +3,53 @@ import customtkinter
 from services.file_service import FileService
 
 class App(customtkinter.CTk):
-    window_width = 700
-    window_height = 350
-    window_title = "Valheim Backup Utility"
+    WINDOW_HEIGHT = 700
+    WINDOW_WIDTH = 350
+    WINDOW_TITLE = "Valheim Backup Utility"
     
     def __init__(self):
         super().__init__()
-
-        # Window config
-        self.title(self.window_title)
-        self.geometry(f"{self.window_width}x{self.window_height}+800+400")
-        # self.minsize(self.window_width, self.window_height)
-        # self.maxsize(self.window_width, self.window_height)
+        self._configure_window()
+        self.radio_var = customtkinter.IntVar(master=self, value=0)
+        self._create_widgets()
+        self._layout_widgets()
+        
+    def _configure_window(self):
+        """Configure the main window."""
+        self.title(self.WINDOW_TITLE)
+        self.geometry(f"{self.WINDOW_HEIGHT}x{self.WINDOW_WIDTH}+800+400")
         self.grid_columnconfigure((0, 1, 2), weight=1)
         self.grid_rowconfigure((0, 1, 2, 3), weight=1)
 
-        # Variables 
-        self.radio_var = customtkinter.IntVar(master=self, value=0)
-        
-        # Assets
-        radio_download = customtkinter.CTkRadioButton(
+    def _create_widgets(self):
+        """Create all widgets used in the application."""
+        self.radio_download = customtkinter.CTkRadioButton(
             self,
             text="Download (Start of session)",
             variable=self.radio_var,
             value=1,
         )
-        radio_upload = customtkinter.CTkRadioButton(
+        self.radio_upload = customtkinter.CTkRadioButton(
             self,
             text="Upload (End of session)",
             variable=self.radio_var,
             value=2,
         )
-        button_start = customtkinter.CTkButton(self, text="Start", command=self.button_callback)
-        frame_config = customtkinter.CTkFrame(self)
-        frame_status = customtkinter.CTkFrame(self)
+        self.button_start = customtkinter.CTkButton(self, text="Start", command=self.on_start_button_click)
+        self.frame_config = customtkinter.CTkFrame(self)
+        self.frame_status = customtkinter.CTkFrame(self)
         
-        # Layout
-        radio_download.grid(row=0, column=0, padx=20, pady=(15, 5), ipady=10, sticky="we")
-        radio_upload.grid(row=1, column=0, padx=20, pady=(5, 15), ipady=10, sticky="we")
-        button_start.grid(row=2, column=1, padx=20, pady=15, ipady=10, sticky="w")
-        
-        frame_config.grid(row=0, column=2, rowspan=4, padx=10, pady=10, sticky="nse")
-        
-        frame_status.grid(row=3, column=0, columnspan=2, padx=10, pady=(5, 10), sticky="nswe")
-        
-    # functions
-    def button_callback(self):
-        action = self.get_action()
+    def _layout_widgets(self):
+        """Arrange widgets in the application window."""
+        self.radio_download.grid(row=0, column=0, padx=20, pady=(15, 5), ipady=10, sticky="we")
+        self.radio_upload.grid(row=1, column=0, padx=20, pady=(5, 15), ipady=10, sticky="we")
+        self.button_start.grid(row=2, column=1, padx=20, pady=15, ipady=10, sticky="w")
+        self.frame_config.grid(row=0, column=2, rowspan=4, padx=10, pady=10, sticky="nse")
+        self.frame_status.grid(row=3, column=0, columnspan=2, padx=10, pady=(5, 10), sticky="nswe")
+    
+    def on_start_button_click(self):
+        """Handle the Start button click event."""
+        action = self.radio_var.get()
         if action == 1:
             FileService.sync_files('download')
         elif action == 2:
@@ -57,9 +57,6 @@ class App(customtkinter.CTk):
         else:
             return None
     
-    def get_action(self):
-        return self.radio_var.get()
-
 # run the application
 def run():
     app = App()
